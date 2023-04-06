c888d166dbb618bf9f699cb4ed00be26c27c82ce
Your Name <you@example.com>
2023-04-06 00:23:06 -0700

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
