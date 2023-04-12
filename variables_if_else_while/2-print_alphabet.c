edeb9a8d94767390f4393e0c021efb82b48ddf1c
Your Name <you@example.com>
2023-04-12 03:28:47 -0700

    putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints alphabet in lower case
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz"
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(apl[i]);
+	}
+	putchar('\n');
+	return (0);
+}
