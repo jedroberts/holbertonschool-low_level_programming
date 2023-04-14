eb2cbf9d581bcec23f4d2817783ad547690816b3
Your Name <you@example.com>
2023-04-14 03:03:34 -0700

    please

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints alphabet in lower case
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z', ch++;)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z', ch++;)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
