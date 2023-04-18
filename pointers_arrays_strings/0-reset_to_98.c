80c092fcacc79401434ca10f74ee648b9d1bad14
Your Name <you@example.com>
2023-04-17 23:18:16 -0700

    first pointer

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- *
- * main - Function that takes a pointer to an int as parameter
+ * reset_to_98 - Function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
  * @n: input
  * Return: n
