6a7e7173c890494ea396241e10878d1eb7280fde
Your Name <you@example.com>
2023-04-23 01:50:32 -0700

    test

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: string to be printed
+ * Return: printed string
+ */
+void _puts(char *str)
+{
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	printf("%s\n", str);
+}
