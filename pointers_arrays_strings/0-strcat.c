450a5fcb9ef0ad815322439371e8f256cc732d39
Your Name <you@example.com>
2023-05-07 04:33:53 -0700

    my first concatenating attempt

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - Function that appends src string
+ * to end of dest string
+ * @dest: string 1
+ * @src: string 2
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *result = dest;
+
+	while (*dest != '\0')
+	{
+		dest = dest + 1;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest = dest + 1;
+		src = src + 1;
+	}
+	*dest = '\0';
+	return (dest);
+}
