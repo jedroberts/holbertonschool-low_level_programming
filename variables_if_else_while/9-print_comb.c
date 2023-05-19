710f1b8e3b4758f5d9070f154aa942a21da96143
Your Name <you@example.com>
2023-05-19 00:51:38 -0700

    whole directory

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single numbers
+ *
+ * Return: awlays 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	if (n = '0'; n <= '8'; n++)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
